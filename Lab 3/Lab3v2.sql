39791f41380e5a669a579c5082390aff447d8b8d
Zachary Fong <zachary.fong1@marist.edu>
2016-02-10 21:17:32 -0500

    Lab3 Fixed
    Statements 5,6,7,8,9 fixed

---
+++ Lab 3/Lab3v2.sql
@@ -0,0 +1,43 @@
+﻿-- 1 --
+select ordno, dollars
+from orders;
+
+--2--
+select name, city
+from agents
+where name in ('Smith');
+
+--3--
+select pid,name, priceUSD
+from products
+where quantity > 208000;
+
+--4--
+select name, city
+from customers
+where city = 'Dallas';
+
+--5--
+select *
+from agents
+where city NOT IN ('Tokyo', 'New York');
+
+--6--
+select *
+from products
+where city NOT IN ('Dallas', 'New York') and priceUSD >= 1;
+
+--7--
+select *
+from orders
+where mon in ('jan', 'mar');
+
+--8--
+select *
+from orders
+where mon in ('feb') and dollars < 500;
+
+--9--
+select *
+from orders
+where cid = 'c005';
