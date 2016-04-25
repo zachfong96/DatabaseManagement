f34b8e69dfab49f3b0032b8a6a931523b4522f6a
Zachary Fong <zachary.fong1@marist.edu>
2016-04-24 21:49:31 -0400

    Lab 9
    finished lab 9

---
+++ Lab 9/lab9.sql
@@ -0,0 +1,60 @@
+﻿create table players(
+	pid char(4) not null,
+	fname text, 
+	lname text,
+	address text,
+	phonenum bigint,
+Primary key (pid)
+);
+
+create table AssistantCoach (
+	aid char(4) not null,
+	fname text,
+	lname text, 
+	address text, 
+	phonenum bigint,
+	yearscoached int,
+primary key (aid)
+);
+
+create table headcoach (
+	cid char(4) not null,
+	fname text,
+	lname text,
+	address text,
+	phonenum bigint, 
+	yearscoached int,
+primary key (cid)
+);
+
+create table league_under_10 (
+	le_id char(4) not null,
+	pid char(4) not null references players(pid),
+	cid char(4)not null references headcoach(cid),
+	aid char(4) not null refernces AssistantCoach(aid),
+primary key (le_id)
+);
+
+create table league_10 (
+	lea_id char(4) not null,
+	pid char(4) not null references players(pid),
+	cid char(4)not null references headcoach(cid),
+	aid char(4) not null refernces AssistantCoach(aid),
+primary key (le_id)
+);
+
+create table league_10_through_14 (
+	leag_id char(4) not null,
+	pid char(4) not null references players(pid),
+	cid char(4)not null references headcoach(cid),
+	aid char(4) not null refernces AssistantCoach(aid),
+primary key (le_id)
+);
+
+create table league_over_14 (
+	lid char(4) not null,
+	pid char(4) not null references players(pid),
+	cid char(4)not null references headcoach(cid),
+	aid char(4) not null refernces AssistantCoach(aid),
+primary key (le_id)
+);
